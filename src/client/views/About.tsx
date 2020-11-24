a7bf0ede55e0cf6b4dc03d9a80492cb0a9bb064f
Nicole Pasquale <NSquale212@gmail.com>
2020-11-23 18:59:13 -0600

    Refactor responsiveness on about us page

--- src/client/views/About.tsx
+++ src/client/views/About.tsx
@@ -1,15 +1,15 @@
 import * as React from 'react';
-import NavBar from '../components/navigation/NavComponent';
 import styled from 'styled-components';
+import NavBar from '../components/navigation/NavComponent';
 import GoogleFontLoader from 'react-google-font-loader';
 
-const About: React.FC<IAboutProps> = () => {
 
-    return (
+const About = () => {
+    return(
         <Background>
             <NavBar />
             <main className="container-fluid">
-                <GoogleFontLoader
+            <GoogleFontLoader
                     fonts={[
                         {
                             font: 'Homemade Apple',
@@ -23,39 +23,39 @@ const About: React.FC<IAboutProps> = () => {
                     subsets={['cyrillic-ext', 'greek']}
                 />
                 <header className="d-flex justify-content-center">
-                    <StyledTitle className="m-5 text-muted">About Us</StyledTitle>
+                    <StyledTitle className="m-5">About Us</StyledTitle>
                 </header>
-                <section className="row justify-content-center">
-                    <OvalDiv>
-                        <StyledUl className="bg-transparent">
+                <CenterDiv>
+                    <OvalDiv className="d-flex justify-content-center align-items-center">
+                        <StyledUl className="m-2">
                             <StyledLi>
                                 <StyledImg className="mr-4" src="/images/draw.png" alt="colored pencil icon" />
                                 Arts & Crafts Daily
-                        </StyledLi>
+                            </StyledLi>
                             <StyledLi className="mt-2 mb-2">
                                 <StyledImg className="mr-4" src="/images/house.png" alt="house icon" />
                                 Comfortable Home Setting
-                        </StyledLi>
+                            </StyledLi>
                             <StyledLi>
                                 <StyledImg className="mr-4" src="/images/cpr.png" alt="cpr certified icon" />
                                 CPR Certified Staff
-                        </StyledLi>
+                            </StyledLi>
                             <StyledLi className="mt-2 mb-2">
                                 <StyledImg className="mr-4" src="/images/takeaway.png" alt="lunch icon" />
                                 Meals included in tuition
-                        </StyledLi>
+                            </StyledLi>
                             <StyledLi>
                                 <StyledImg className="mr-4" src="/images/flag.png" alt="spanish flag icon" />
                                 Spanish Immersion Program
-                        </StyledLi>
+                            </StyledLi>
                             <StyledLi>
                                 <StyledImg className="mr-4" src="/images/cleaning.png" alt="cleaning supplies icon" />
                                 Covid Compliant Practices
                             </StyledLi>
                         </StyledUl>
                     </OvalDiv>
-                </section>
-                <section className="mt-5">
+                </CenterDiv>
+                <TextContainer>
                     <div className="row justify-content-center">
                         <article className="col-md-5">
                             <p className="text-justify">
@@ -68,57 +68,13 @@ const About: React.FC<IAboutProps> = () => {
                             </p>
                         </article>
                     </div>
-                </section>
+                </TextContainer>
             </main>
         </Background>
     );
 }
 
-const OvalDiv = styled.div`
-    background: white;
-    border-radius: 50%;
-    padding: 3.5rem;
-    border: 4px solid #b2d0f1;
-
-    @media (max-width: 812px) {
-        padding: 2rem;
-    }
-`;
-
-const StyledTitle = styled.h1`
-    font-family: 'Beth Ellen', sans-serif;
-    font-size: 40px;
-`;
-
-const StyledUl = styled.ul`
-    list-style: none;
-    column-count: 2;
-    
-    @media (max-width: 425px) {
-        column-count: 1;
-    }
-`;
-
-const StyledLi = styled.li`
-    font-family: 'Beth Ellen', sans-serif;
-    font-size: 16px;
-
-    @media (max-width: 812px) {
-        font-size: 11px;
-    }
-`;
-
-const StyledImg = styled.img`
-    width: 48px;
-    height: 48px;
-    
-    @media (max-width: 812px) {
-        width: 20px;
-        height: 20px;
-    }
-`;
-
-const Background = styled.div `
+const Background = styled.div`
     background-color: #b2d0f1;
     height: 50%;
     background-attachment: fixed;
@@ -127,27 +83,114 @@ const Background = styled.div `
     background-size: cover;
     width: 100%;
 
-    @media (max-width: 375px) and (orientation: portrait) {
-        height: 45%;
-    }
-
-    @media (max-height: 667px) and (orientation: portrait) {
-        height: 55%;
-    }
-
-    @media (max-width: 812px) and (orientation: landscape) {
-        height: 90%;
-    }
-
-    @media (min-width: 768px) and (max-width: 1024px) and (orientation: portrait) {
-        height: 32%;
-    }
-
-    @media (min-width: 1025px) and (max-width: 1366px) and (orientation: landscape) {
-        height: 38%;
+    @media (min-width: 667px) and (max-width: 812px) and (orientation: landscape) {
+        height: 60%;
     }
 `;
 
-export interface IAboutProps {}
+const StyledTitle = styled.h1`
+    font-family: 'Beth Ellen', sans-serif;
+    font-size: 40px;
+    position: absolute;
+    top: 15%;
+
+    @media (max-width: 812px) and (orientation: landscape) {
+        display: none;
+    }
+`;
+
+const CenterDiv = styled.section`
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
+    left: 20%;
+    right: 20%;
+
+    @media (max-width: 470px) {
+        left: 5%;
+        right: 5%;
+    }
+
+    @media (min-width: 667px) and (max-width: 812px) and (orientation: landscape) {
+        top: 70%;
+        transform: translateY(-70%);
+        left: 5%;
+        right: 5%;
+    }
+
+    @media (min-width: 470px) and (max-width: 1300px) {
+        left: 12%;
+        right: 12%;
+    }
+`;
+
+const OvalDiv = styled.div`
+    height: 30vh;
+    background-color: #FFFFFF;
+    border-radius: 50%;
+    border: 4px solid #b2d0f1;
+
+    @media (min-width: 667px) and (max-width: 812px) and (orientation: landscape) {
+        height: 50vh;
+    }
+`;
+
+const StyledUl = styled.ul`
+    list-style: none;
+    column-count: 2;
+    
+    @media (max-width: 768px) and (orientation: portrait) {
+        column-count: 1;
+    }
+`;
+
+const StyledLi = styled.li`
+    font-family: 'Beth Ellen', sans-serif;
+    font-size: 16px;
+
+    @media (min-width: 768px) and (max-width: 1023px) {
+        font-size: 14px;
+    }
+
+    @media (max-width: 1024px) and (orientation: portrait) {
+        font-size: 18px;
+    }
+
+    @media (max-width: 767px) {
+        font-size: 11px;
+    }
+
+    @media (min-width: 668px) and (max-width: 812px) and (orientation: landscape) {
+        font-size: 12px;
+    }
+
+    @media (max-width: 667px) and (orientation: landscape) {
+        font-size: 10px;
+    }
+`;
+
+const StyledImg = styled.img`
+    width: 48px;
+    height: 48px;
+    
+    @media (max-width: 1024px) {
+        width: 20px;
+        height: 20px;
+    }
+
+    @media (max-width: 667px) and (orientation: landscape) {
+        width: 16px;
+        height: 16px;
+    }
+`;
+
+const TextContainer = styled.section`
+    position: absolute;
+    top: 70%;
+
+    @media (min-width: 667px) and (max-width: 812px) and (orientation: landscape) {
+        top: 95%;
+    }
+`;
 
 export default About;
