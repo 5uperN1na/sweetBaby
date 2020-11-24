35229d8b6c6f8e1379948a03a7e54f116efd4add
PawelPavo <Pawelebllc@gmail.com>
2020-11-24 16:58:29 -0500

    fixed gallery, added CSS

--- src/client/views/Gallery.tsx
+++ src/client/views/Gallery.tsx
@@ -1,12 +1,14 @@
 import React, { useState, useCallback } from "react";
-// import { photos } from "./photos";
+import Gallery from "react-photo-gallery";
 import Carousel, { Modal, ModalGateway } from "react-images";
+import { photos } from "./photos";
+import styled from "styled-components";
+import GoogleFontLoader from "react-google-font-loader";
+import NavBar from "../components/navigation/NavComponent";
 
-
-const Gallery: React.FC<IGalleryProps> = (props) => {
-
-  const [currentImage, setCurrentImage] = useState<number>(0);
-  const [viewerIsOpen, setViewerIsOpen] = useState<boolean>(false);
+const GalleryView: React.FC<IGalleryViewProps> = (props) => {
+  const [currentImage, setCurrentImage] = useState(0);
+  const [viewerIsOpen, setViewerIsOpen] = useState(false);
 
   const openLightbox = useCallback((event, { photo, index }) => {
     setCurrentImage(index);
@@ -18,81 +20,87 @@ const Gallery: React.FC<IGalleryProps> = (props) => {
     setViewerIsOpen(false);
   };
 
-  const photos = [
-    {
-      src: "./images/baby.jpg",
-      width: 3,
-      height: 4
-    },
-    {
-      src: "./images/baby_boy_play.jpg",
-      width: 3,
-      height: 4
-    },
-    {
-      src: "./images/happy_baby.jpg",
-      width: 3,
-      height: 4
-    },
-    {
-      src: "./images/boy_carpet.jpg",
-      width: 3,
-      height: 4
-    },
-    {
-      src: "./images/boy2.jpg",
-      width: 3,
-      height: 4
-    },
-    {
-      src: "./images/glendaboy.jpg",
-      width: 3,
-      height: 4
-    },
-    {
-      src: "./images/little_boy.jpg",
-      width: 3,
-      height: 4
-    },
-    {
-      src: "./images/playroom.jpg",
-      width: 4,
-      height: 3
-    },
-    {
-      src: "./images/vivi2.jpg",
-      width: 2,
-      height: 3
-    }
-  ];
-
   return (
     <div>
-      <Gallery photos={photos} onClick={openLightbox} />
-      <ModalGateway>
-        {viewerIsOpen ? (
-          <Modal onClose={closeLightbox}>
-
-            <Carousel
-              currentIndex={currentImage}
-              views={photos.map(x => ({
-                ...x,
-                srcset: x.srcSet,
-                caption: x.title
-              }))}
-            />
-
-          </Modal>
-        ) : null}
-      </ModalGateway>
+      <Background>
+        <NavBar />
+        <GoogleFontLoader
+          fonts={[
+            {
+              font: 'Homemade Apple',
+              weights: [400, '400i'],
+            },
+            {
+              font: 'Beth Ellen',
+              weights: [400, '400i'],
+            },
+          ]}
+          subsets={['cyrillic-ext', 'greek']}
+        />
+        <StyledMain className="container">
+          <div className="row d-flex justify-content-center align-items-center">
+            <StyledTitle className="my-5 text-muted">Contact</StyledTitle>
+          </div>
+          <Gallery photos={photos} onClick={openLightbox}/>
+          <ModalGateway>
+            {viewerIsOpen ? (
+              <Modal onClose={closeLightbox}>
+                <Carousel
+                  currentIndex={currentImage}
+                  views={photos.map<any>(photo => ({
+                    ...photo,
+                  }))}
+                />
+              </Modal>
+            ) : null}
+          </ModalGateway>
+        </StyledMain>
+      </Background>
     </div>
   );
 }
 
+const StyledTitle = styled.h1`
+    font-family: 'Beth Ellen', sans-serif;
+    font-size: 40px;
+`;
 
-export interface IGalleryProps { }
+const StyledMain = styled.main`
+    margin-top: 9.375rem;
 
-export default Gallery;
+    @media (max-width: 812px) and (orientation: landscape) {
+        margin-top: 4.375rem;
+    }
+
+    img {
+      opacity: 0.75;
+      transition: all .4s ease;
+      -webkit-transition: all .4s ease; 
+      padding: 5px;
+    }
+
+    img:hover {
+      transform: scale(1.05);
+      box-shadow: 0 10px 20px rgba(0, 0, 0, 1), 0 4px 8px rgba(0, 0, 0, 1);
+      opacity: 1;
+
+    }
+`;
+
+const Background = styled.div`
+    background-color: #f5dcdb;
+    height: 55%;
+    background-attachment: fixed;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    width: 100%;
+`;
+
+
+export interface IGalleryViewProps { }
+
+export default GalleryView;
 
 
 
