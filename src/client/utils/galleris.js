fe1eab69b7e25d994b71eb3f2c2e53a2cfe344c9
PawelPavo <Pawelebllc@gmail.com>
2020-11-30 11:23:38 -0500

    added one more gallery for testing - all good

--- src/client/utils/galleris.js
+++ src/client/utils/galleris.js
@@ -214,5 +214,133 @@ export const galleries = [
           height: 4
         },
       ]
+    },
+    {
+      "id": 4,
+      "name": "Playtime",
+      "photos": [
+ 
+        {
+          src: "../images/playroom.jpg",
+          width: 5,
+          height: 4
+        },
+        {
+          src: "../images/playtime2.jpg",
+          width: 3,
+          height: 4
+        },
+        {
+          src: "../images/playtime3.jpg",
+          width: 3,
+          height: 4
+        },
+    
+        {
+          src: "../images/playtime4.jpg",
+          width: 3,
+          height: 4
+        },
+    
+        {
+          src: "../images/playtime5.jpg",
+          width: 3,
+          height: 4
+        },
+        {
+          src: "../images/playtime6.jpg",
+          width: 4,
+          height: 4
+        },
+        {
+          src: "../images/playtime7.jpg",
+          width: 3,
+          height: 4
+        },
+        {
+          src: "../images/playtime8.jpg",
+          width: 3,
+          height: 4
+        },
+        {
+          src: "../images/playtime9.jpg",
+          width: 3,
+          height: 4
+        },
+        {
+          src: "../images/playtime10.jpg",
+          width: 3,
+          height: 4
+        },
+    
+        {
+          src: "../images/playtime11.jpg",
+          width: 3,
+          height: 4
+        },
+    
+        {
+          src: "../images/playtime12.jpg",
+          width: 3,
+          height: 4
+        },
+    
+        {
+          src: "../images/playtime13.jpg",
+          width: 3,
+          height: 4
+        },
+    
+    
+        {
+          src: "../images/playtime14.jpg",
+          width: 3,
+          height: 4
+        },
+    
+        {
+          src: "../images/playtime15.jpg",
+          width: 3,
+          height: 4
+        },
+    
+        {
+          src: "../images/playtime16.jpg",
+          width: 3,
+          height: 4
+        },
+    
+    
+        {
+          src: "../images/playtime17.jpg",
+          width: 3,
+          height: 4
+        },
+    
+        {
+          src: "../images/playtime18.jpg",
+          width: 3,
+          height: 4
+        },
+    
+        {
+          src: "../images/playtime19.jpg",
+          width: 3,
+          height: 4
+        },
+    
+        {
+          src: "../images/playtime20.jpg",
+          width: 3,
+          height: 4
+        },
+    
+        {
+          src: "../images/playtime21.jpg",
+          width: 3,
+          height: 4
+        },
+    
+      ]
     }
   ];
