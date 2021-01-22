5d7760acb3bf5e9ab52e111f8377d36696f64a51
Paula Suarez <pauladzuck@hotmail.com>
2021-01-22 12:09:32 -0600

    Adding final updates

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -16,46 +16,48 @@ import PageNotFound from './views/PageNotFound'
 
 const App = () => {
 	return (
-		<BrowserRouter>
-			<Switch>
-				<Route exact path="/">
-					<Home />
-				</Route>
+	
+			<BrowserRouter>
+				<Switch>
+					<Route exact path="/">
+						<Home />
+					</Route>
 
-				<Route exact path="/about">
-					<About />
-				</Route>
+					<Route exact path="/about">
+						<About />
+					</Route>
 
-				<Route exact path="/testimonials">
-					<Testimonials />
-				</Route>
+					<Route exact path="/testimonials">
+						<Testimonials />
+					</Route>
 
-				<Route exact path="/contact">
-					<Contact />
-				</Route>
-				{/* <Route exact path="/gallery/:id">
+					<Route exact path="/contact">
+						<Contact />
+					</Route>
+					{/* <Route exact path="/gallery/:id">
 					<GalleryDetails />
 				</Route> */}
 
-				{/* <Route exact path="/gallery">
+					{/* <Route exact path="/gallery">
 					<GalleryView />
 				</Route> */}
 
-				<Route exact path="/gallery">
-					<Gallery />
-				</Route>
+					<Route exact path="/gallery">
+						<Gallery />
+					</Route>
 
-				<Route exact path="/schedule2">
-					<Schedule2 />
-				</Route>
-				<Route path="*" component={PageNotFound} />
+					<Route exact path="/schedule2">
+						<Schedule2 />
+					</Route>
+					<Route path="*" component={PageNotFound} />
 
-				<Route exact path="/pagenotfound">
-					<PageNotFound />
-				</Route>
+					<Route exact path="/pagenotfound">
+						<PageNotFound />
+					</Route>
 
-			</Switch>
-		</BrowserRouter>
+				</Switch>
+			</BrowserRouter>
+		
 	)
 };
 
